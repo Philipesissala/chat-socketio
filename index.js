391f47dcb0d8a1c7e9ffcd45170e0a5b6d76a3fc
Philipesissala <filipesissala14@gmail.com>
2022-08-14 13:37:07 +0100

    Carregando a base do projeto

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => console.log(`User ${socket.id} disconnected`));
+
+  socket.on("message", (data) => {
+    console.log(data);
+  });
+});
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+http.listen(3000, () => console.log("Running..."));
